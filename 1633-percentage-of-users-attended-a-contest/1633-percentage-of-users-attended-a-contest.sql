b1c1d177dfbc2468b63bbdd20f9868442c9eb7ce
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-08-21 09:51:10 +0900

    Time: 1002 ms (47.87%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,7 @@
 SELECT
     r.contest_id,
     ROUND(COUNT(r.user_id) / (SELECT COUNT(user_id)
-                       FROM Users) * 100, 2) percentage
+                              FROM Users) * 100, 2) percentage
 FROM Register r
     INNER JOIN Users u
     ON r.user_id = u.user_id
