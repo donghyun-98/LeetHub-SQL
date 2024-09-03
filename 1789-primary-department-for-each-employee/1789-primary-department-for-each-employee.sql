e707ec0ed26d4dd838a66d93feb5af0c11199faa
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-09-03 11:20:49 +0900

    Time: 534 ms (49.68%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,11 +1,9 @@
-SELECT 
-    employee_id,
-    department_id
-FROM Employee
-WHERE primary_flag = 'Y' or
-    employee_id IN (
-        SELECT employee_id
-        FROM Employee
-        GROUP BY employee_id
-        HAVING COUNT(employee_id) = 1
-        );
+SELECT employee_id, department_id
+FROM Employee 
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id, department_id
+FROM Employee 
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+ORDER BY employee_id;
