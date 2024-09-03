961f2733a4ae10afefff81cf1153ecdf963e1269
Donghyun Shin <ehdtkd98@korea.ac.kr>
2024-09-03 10:45:41 +0900

    Time: 521 ms (96.13%), Space: 0B (100.00%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,10 @@
+SELECT
+    mgr.employee_id,
+    mgr.name,
+    COUNT(emp.employee_id) as reports_count,
+    ROUND(AVG(emp.age)) as average_age
+FROM Employees emp
+    JOIN Employees mgr
+    ON emp.reports_to = mgr.employee_id
+GROUP BY employee_id
+ORDER BY employee_id
